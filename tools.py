6a94a6c4ebdc52c0c305db1ab8633e9a699a1585
Sean Bryan <seanbryan.noces@gmail.com>
2026-05-07 23:41:36 +0800

    created the tools that the agent will use

--- tools.py
+++ tools.py
@@ -1,29 +1,63 @@
-import json, os, requests
+import json, os
 
-def safeDivide(a,b):
-    try:
-        return a/b
+allowed_ch = "0123456789+-()*/^% ."
+
+#main function
+def safeCalculate(expr: str):
+    conv_expr = expr.replace("^", "**")
+    try: 
+        for ch in conv_expr:
+            if ch not in allowed_ch:
+                return "Invalid Expression"
+        # We use a dict for the result to safely handle the eval return
+        return eval(conv_expr, {"__builtins__": None})
     except ZeroDivisionError:
-        return "Cannot divide a number to zero"
+        return "Math Error"
+    except Exception:
+        return "Invalid Expression"
+
+def calculate(expression: str) -> int | float | str:
     
-print(safeDivide(1,0))
+    return safeCalculate(expression)
 
-allowed_ch="0123456789+-()*/**%."
-def safeCalculate(expr):
-    for ch in expr:
-        if(ch not in allowed_ch):
-            return "Invalid Expression"
-    return eval(expr, {"__builtins__": None})
+#getting the weather data
+def get_weather(city:str):
+    fp="/home/competitor/data/weather_data.json"
+    
+    try:
+        with open(fp, 'r') as f:
+            data=json.load(f)
+        
+        for d in data:
+            if(d.get('city').lower() == city.lower()):
+                temp=d.get('temperature')
+                cond=d.get('condition')
+                
+                if(temp is None or cond is None):
+                    return "Weather Data not found"
+                
+                return f"{d['city']}: {temp} °C, {cond}"
+        return "Weather Data not found"
+    except (FileNotFoundError, json.JSONDecodeError):
+        return "Weather Data not found"
 
-print(safeCalculate("3.2-9_8"))
-#using requests
-payload={
-    "model":"mistral",
-    "prompt": "i have allergic rhinitis what are the top recommended prescription drugs by the best allegologists",
-    "stream": False
-}
-ollama_api="http://localhost:11434/api/generate"
-r=requests.post(ollama_api, json=payload)
-data=r.json()
-print(data['response'])
+#file reader
+def read_file(filename:str):
+    base_dir="/home/competitor/data/files/"
+    abs_base_dir=os.path.abspath(base_dir)
+    
+    #requested directory
+    req_dir=os.path.abspath(os.path.join(abs_base_dir, filename))
+    
+    if not req_dir.startswith(abs_base_dir):
+        return "Restricted path"
+    
+    try:
+        with open(req_dir, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "File not found"
+    except Exception as e:
+        return str(e)
 
+print(read_file("tex.txt"))
